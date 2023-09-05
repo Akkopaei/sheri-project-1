2391677e4e0bf0df5ad3b11f4d8d5ca87e41f18e
Akkopaei <amansaifi0699@gmail.com>
2023-09-05 23:29:06 +0530

    conforming

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('.main'),
     smooth: true
 });
+
+
+function mouseFollowCircle(){
+    window.addEventListener("mousemove",(dets)=>{
+        document.querySelector('#circle').style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+mouseFollowCircle();
