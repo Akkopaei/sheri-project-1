4698340ee6d1c1aff95da41da3f24b8f28413dca
Akkopaei <amansaifi0699@gmail.com>
2023-09-07 21:46:27 +0530

    finally fixed the issue

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const scroll = new LocomotiveScroll({
 
 
 function mouseFollowCircle(){
-    window.addEventListener("mousemove",(dets)=>{
-        document.querySelector('#circle').style.transform = `translate(${dets.clientX-7}px,${dets.clientY-7}px)`
+     window.addEventListener("mousemove",(dets)=>{
+        document.querySelector('#circle').style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`
     })
 }
 
@@ -42,29 +42,29 @@ animation();
 mouseFollowCircle();
 
 
-document.querySelectorAll(".elem").forEach(function (elem){
-    let rotate = 0;
-    let diffrence = 0;
-
-    elem.addEventListener("mouseleave", function (){
-        gsap.to(elem.querySelector("img"),{
-            opacity: 0,
-            ease: Power3,
-            duration:.5
-        })
-    })
-
-    elem.addEventListener("mousemove", function (dets){
-        let diff = dets.clientX - elem.getBoundingClientRect().top;
-         diffrence = dets.clientX - rotate;
-         rotate = dets.clientX
-        gsap.to(elem.querySelector("img"),{
-            opacity: 1,
-            ease: Power1,
-            top: diffrence,
-            left: dets.clientX,
-            rotate: gsap.utils.clamp(-20,20, diffrence * .8)
-        });
+document.querySelectorAll(".elem").forEach(function (elem) {
+    var rotate = 0;
+    var diffrot = 0;
+  
+    elem.addEventListener("mouseleave", function (dets) {
+      gsap.to(elem.querySelector("img"), {
+        opacity: 0,
+        ease: Power3,
+        duration: 0.5,
+      });
     });
-});
+  
+    elem.addEventListener("mousemove", function (dets) {
+      var diff = dets.clientY - elem.getBoundingClientRect().top;
+      diffrot = dets.clientX - rotate;
+      rotate = dets.clientX;
+      gsap.to(elem.querySelector("img"), {
+        opacity: 1,
+        ease: Power3,
+        top: diff,
+        left: dets.clientX,
+        rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+      });
+    });
+  });
 
